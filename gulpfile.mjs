ac04f6b79ce46791e05cd20b24fbd10931d586c7
Craz1k0ek <bram_kolkman@hotmail.com>
2024-11-22 14:10:38 +0100

    Dependencies
    * Updated the dependencies
    * Updated the Gulp file

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,16 +1,19 @@
-const gulp = require("gulp");
-const { parallel } = require("gulp");
-const sass = require('gulp-sass');
-const cleancss = require('gulp-clean-css');
-const csscomb = require('gulp-csscomb');
-const rename = require('gulp-rename');
-const pug = require('gulp-pug');
-const autoprefixer = require('gulp-autoprefixer');
+import gulp from 'gulp';
+import { parallel } from 'gulp';
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
+import cleancss  from 'gulp-clean-css';
+import csscomb  from 'gulp-csscomb';
+import rename  from 'gulp-rename';
+import pug  from 'gulp-pug';
+import autoprefixer  from 'gulp-autoprefixer';
 
-function build() {
+const sass = gulpSass(dartSass);
+
+export function build() {
   return gulp
     .src('./src/*.scss')
-    .pipe(sass({outputStyle: 'compact', precision: 10})
+    .pipe(sass({outputStyle: 'compressed', precision: 10})
       .on('error', sass.logError)
     )
     .pipe(autoprefixer())
@@ -26,7 +29,7 @@ function build() {
 function docs_css() {
   return gulp
     .src(['./src/*.scss', './docs/src/scss/*.scss'])
-    .pipe(sass({outputStyle: 'compact', precision: 10})
+    .pipe(sass({outputStyle: 'compressed', precision: 10})
       .on('error', sass.logError)
     )
     .pipe(autoprefixer())
@@ -48,12 +51,12 @@ function docs_pug() {
     .pipe(gulp.dest('./docs/'));
 }
 
-function watch() {
+export function watch() {
   gulp.watch('./**/*.scss', parallel(build, docs_css));
   gulp.watch('./**/*.pug', docs_pug);
 }
 
-exports.watch = watch;
-exports.build = build;
-exports.docs = parallel(docs_pug, docs_css);
-exports.default = build;
+// export const watch = watch;
+// export const build = build;
+export const docs = parallel(docs_pug, docs_css);
+export default build;
