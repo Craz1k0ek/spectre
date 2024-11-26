a2edaee142e4cb9e1a7aaa488537b5eeae08d5c1
Craz1k0ek <bram_kolkman@hotmail.com>
2024-11-26 10:39:52 +0100

    WIP
    * Did some stuff, not sure if correct

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,62 +1,55 @@
 import gulp from 'gulp';
-import { parallel } from 'gulp';
-import gulpSass from 'gulp-sass';
-import dartSass from 'sass';
-import cleancss  from 'gulp-clean-css';
-import csscomb  from 'gulp-csscomb';
-import rename  from 'gulp-rename';
-import pug  from 'gulp-pug';
-import autoprefixer  from 'gulp-autoprefixer';
+import * as sass from 'sass'; // Dart Sass API
+import autoprefixer from 'gulp-autoprefixer';
+import cleancss from 'gulp-clean-css';
+import rename from 'gulp-rename';
+import pug from 'gulp-pug';
 
-const sass = gulpSass(dartSass);
-
-export function build() {
+// Compile Sass files
+function buildStyles() {
   return gulp
-    .src('./src/*.scss')
-    .pipe(sass({outputStyle: 'compressed', precision: 10})
-      .on('error', sass.logError)
+    .src('./src/**/*.scss', { sourcemaps: true })
+    .pipe(
+      gulp.dest((file) => {
+        // Use Dart Sass API to compile the file manually
+        const result = sass.compile(file.path, {
+          style: 'compressed', // Use 'expanded' for readable output or 'compressed' for minified
+          precision: 10,
+        });
+        file.contents = Buffer.from(result.css);
+        return './dist'; // Target directory
+      })
     )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(autoprefixer({ cascade: false })) // Add vendor prefixes
+    .pipe(gulp.dest('./dist', { sourcemaps: '.' })) // Write output with sourcemaps
+    .pipe(cleancss()) // Minify CSS
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
     .pipe(gulp.dest('./dist'));
 }
 
-function docs_css() {
+// Compile Pug templates
+function buildPug() {
   return gulp
-    .src(['./src/*.scss', './docs/src/scss/*.scss'])
-    .pipe(sass({outputStyle: 'compressed', precision: 10})
-      .on('error', sass.logError)
+    .src('./docs/src/**/!(_)*.pug')
+    .pipe(
+      pug({
+        pretty: true, // For readable HTML
+      })
     )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./docs/dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./docs/dist'));
-}
-
-function docs_pug() {
-  return gulp
-    .src('docs/src/**/!(_)*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
     .pipe(gulp.dest('./docs/'));
 }
 
-export function watch() {
-  gulp.watch('./**/*.scss', parallel(build, docs_css));
-  gulp.watch('./**/*.pug', docs_pug);
+// Watch for file changes
+function watchFiles() {
+  gulp.watch('./src/**/*.scss', buildStyles); // Watch SCSS files
+  gulp.watch('./docs/src/**/*.pug', buildPug); // Watch Pug files
 }
 
-// export const watch = watch;
-// export const build = build;
-export const docs = parallel(docs_pug, docs_css);
+export const build = gulp.series(buildStyles);
+export const docs = gulp.series(buildPug);
+export const watch = gulp.parallel(watchFiles);
 export default build;
